b317a6665ed8315dcc51cbfa13e2ff88061e1904
Harish-com <sharmaharish123433@gmail.com>
2022-07-30 17:23:29 +0530

    30/07

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,91 +41,76 @@ window.Echo = new Echo({
 
 
 var notifications = [];
+var channel = window.Echo.channel('admin-notify');
+channel.listen('SendNotification', function (data) {
+    $.get('/latest-notification/', function (new_notifications) {
 
-
-var channel = window.Echo.channel('notify');
-console.log(channel);
-// channel.listen('SendNotification', function(data) {
-//     $.get('/latest-notification/', function (new_notifications) {
-//         if(data.blog.id == new_notifications.data.id){
-//             addNotifications(new_notifications, "#notifications");
-//
-//             //show popup
-//             var app_url = $('#app_url').val();
-//             var blog_detail = '<h3>'+data.blog.title+'</h3><br><img src="'+app_url+'/images/blog/'+data.blog.photo+'" /><p>'+data.blog.description+'</p>';
-//             $('.blog-popup .blog-detail').html(blog_detail);
-//
-//             $('.blog-popup').removeClass("out");
-//             $('.blog-popup').addClass("side-popup");
-//         }
-//     });
-//
-// });
-
-
-const NOTIFICATION_TYPES = {
-    announcement: 'App\\Notifications\\Announcement'
+        if (data.data.id == new_notifications.id) {
+            addNotifications(new_notifications, "#notifications"); //show popup
+            var app_url = $('#app_url').val();
+            var blog_detail = '<h3>' + data.data.name + '</h3><br><p>' + data.data.message + '</p>';
+            $('.blog-popup .blog-detail').html(blog_detail);
+            $('.blog-popup').removeClass("out");
+            $('.blog-popup').addClass("side-popup");
+        }
+    });
+});
+var NOTIFICATION_TYPES = {
+    notify: 'App\\Notifications\\Notify'
 };
-
-
-$(document).ready(function() {
-    $.get('http://127.0.0.1:8000/notifications', function (data) {
+$(document).ready(function () {
+    $.get('/notifications', function (data) {
         addNotifications(data, "#notifications");
     });
 });
 
 function addNotifications(newNotifications, target) {
-    notifications = _.concat(newNotifications, notifications);
-    // show only last 5 notifications
+    notifications = _.concat(newNotifications, notifications); // show only last 5 notifications
+
     notifications = notifications.slice(0, 5);
     showNotifications(notifications, target);
 }
 
 function showNotifications(notifications, target) {
-    if(notifications.length) {
-
-        $('.badge-counter').text(notifications.length);
-
+    if (notifications.length) {
+        $('.number').text(notifications.length);
         var htmlElements = notifications.map(function (notification) {
             return makeNotification(notification);
         });
+        $('#notifications').html(htmlElements);
         $(target + 'Menu').html(htmlElements.join(''));
-        $(target).addClass('has-notifications')
+        $(target).addClass('has-notifications');
     } else {
         $(target + 'Menu').html('<li class="dropdown-header">No notifications</li>');
         $(target).removeClass('has-notifications');
     }
-}
+} // Make a single notification string
 
 
-// Make a single notification string
 function makeNotification(notification) {
     var to = routeNotification(notification);
     var notificationText = makeNotificationText(notification);
-    return '<li><a href="' + to + '">' + notificationText + '</a></li>';
+    return '<li>' + notificationText + '</li>';
 }
 
 function routeNotification(notification) {
-    var to = `?read=${notification.id}`;
-    if(notification.type === NOTIFICATION_TYPES.announcement) {
-        const id = notification.data.id;
-        if(notification.notifiable_type == 'App\\Player'){
-            var guard = 'player';
-        } else if(notification.notifiable_type == 'App\\Store'){
-            var guard = 'store';
-        }
+    var to = "?read=".concat(notification.id);
 
-        to = `${guard}/annoucement_detail/${id}` + to;
+    if (notification.type === NOTIFICATION_TYPES.notify) {
+        var id = notification.data.id;
+        to = "".concat(guard, "/notify/").concat(id) + to;
     }
+
     return '/' + to;
 }
 
 function makeNotificationText(notification) {
-
     var text = '';
-    if(notification.type === NOTIFICATION_TYPES.announcement) {
-        const title = notification.data.title;
-        text += `<strong>${title}</strong>`;
-    }
+    // if (notification.type === NOTIFICATION_TYPES.notify) {
+    //     alert(notification.name);
+    var title = notification.name;
+    text += "<strong>".concat(title, "</strong>");
+    // }
+
     return text;
 }
